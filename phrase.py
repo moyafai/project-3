04ec8ac0d63476370267d0459e40b34469a673a7
moyafai <87585768+moyafai@users.noreply.github.com>
2021-09-07 21:33:21 -0400

    Add files via upload

---
+++ phrase.py
@@ -0,0 +1,28 @@
+# Create your Phrase class logic here.
+from phrasehunter import game
+class Phrase:
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+
+
+    def display(self):
+         for letter in self.active_phrases:
+             if letter in self.guesses:
+                 print(letter)
+             else:
+                 print("_ ") 
+
+
+    def check_guess(self, guess):
+        if guess in self.active_phrases:
+            return True
+        else:
+            return False
+
+
+    def check_complete(self):
+        for letter in self.active_phrases:
+            if letter not in self.active_phrases:
+                return False
+        else:
+            return True                                       
