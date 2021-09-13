317ea21396760242ee07e0cbeff0325903ff1070
moyafai <87585768+moyafai@users.noreply.github.com>
2021-09-12 20:31:37 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+# Import your Game class
+
+# Create your Dunder Main statement.
+
+# Inside Dunder Main:
+## Create an instance of your Game class
+## Start your game by calling the instance method that starts the game loop
+import Game from phrasehunter
+
+if __name__ == "__main__":
+    game = Game()
+    game.start()
